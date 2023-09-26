db35418b753fbd366c11b358f769321aec4e13d4
DanielaSMR <10828775@ieselgrao.org>
2023-09-26 08:41:42 +0200

    guardar

--- java2.java
+++ java2.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+
 public class java2 {
-    
+    public static void main(String[] args){
+        int num1;
+        int num2;
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Escribir primer numero");
+    num1 = sc.nextInt();
+    System.out.println("Escribir segundo numero");
+    num2 = sc.nextInt();
+
+        while(num1!=num2){
+            System.out.println("Escribir primer numero");
+            num1 = sc.nextInt();
+            System.out.println("Escribir segundo numero");
+            num2 = sc.nextInt();
+        }
+    }
 }
